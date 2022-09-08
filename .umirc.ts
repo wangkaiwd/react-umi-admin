ff9d88971640fb80357e7449e4ee17e78f002006
wangkaiwd <1162486176@qq.com>
2022-09-08 22:38:28 +0800

    feat: update server

--- .umirc.ts
+++ .umirc.ts
@@ -31,10 +31,10 @@ export default defineConfig({
     },
   ],
   npmClient: 'pnpm',
-  // proxy: {
-  //   '/api': {
-  //     'target': 'http://localhost:3000',
-  //     'changeOrigin': true,
-  //   },
-  // },
+  proxy: {
+    '/api': {
+      target: 'http://localhost:3000',
+      changeOrigin: true,
+    },
+  },
 });
--- server.js
+++ server.js
@@ -11,4 +11,6 @@ app.use((ctx) => {
   }
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('server is listening on port 3000');
+});
